f66f6d4d3d04ba136012e89a365977f1e98cf91c
Sachin Tyagi <14.sachintyagi@gmail.com>
2024-12-07 00:58:35 +0530

    added

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -3,7 +3,7 @@ import { AiOutlineDown, AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import logo from '../../assets/images/logo.svg'
 import quote from '../../assets/images/quote-icon.svg'
 import quote2 from '../../assets/images/quote-icon2.svg'
-import arrow from '../../assets/images/down_arrow.svg'
+import arrow from '../../assets/images/crossArrow.svg'
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -34,12 +34,12 @@ const Header = () => {
 
                 {/* Quote Request Button and Down Arrow (Hidden on mobile) */}
                 <div className="hidden md:flex items-center">
-                    <button className="bg-black hover:bg-[#1782C5] w-auto h-[48px] text-white px-4 p-2 rounded-full">
+                    <button className="bg-white flex justify-between items-center gap-3 hover:bg-[#1782C5] w-auto h-[48px] px-3 p-2 bg-transparent border border-black rounded-full">
                         Quote Request
-                    </button>
-                    <div className='bg-black hover:bg-[#1782C5] flex items-center justify-center text-white w-[48px] h-[48px] p-1 px-4 rounded-full'>
-                        <img src={arrow} className='w-[20px] h-[20px]' alt="Down arrow icon" />
+                        <div className='bg-black hover:bg-[#1782C5] flex items-center justify-center text-white w-[28px] h-[28px] p-2 rounded-full'>
+                        <img src={arrow} className='' alt="Down arrow icon" />
                     </div>
+                    </button>
                 </div>  
 
                 {/* Mobile Menu Button */}
