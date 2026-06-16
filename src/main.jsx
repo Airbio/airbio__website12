34f65c5dddd25b169eb480ee97567d05849786fd
Ashutosh Mishra <ashutosh12555@gmail.com>
2026-06-16 17:13:36 +0530

    Add prerendering with react-snap for SEO

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import { HelmetProvider } from "react-helmet-async";
-
-// createRoot(document.getElementById('root')).render(
-//   // <StrictMode>
-//   //   <App />
-//   // </StrictMode>,
-//   <StrictMode>
-//     <HelmetProvider>
-//       <App />
-//     </HelmetProvider>
-//   </StrictMode>,
-// )
-
-
 import { StrictMode } from "react";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
